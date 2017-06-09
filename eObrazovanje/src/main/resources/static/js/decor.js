a1908abd357c8665c131a1b5931649858b27acb3
Aleksa <domic.aleksa@gmail.com>
2017-06-09 06:00:25 +0200

    Deo predavaca gotov, fali dodavanje novog kursa za izabrani predmet i celokupno ocenjivanje

--- eObrazovanje/src/main/resources/static/js/decor.js
+++ eObrazovanje/src/main/resources/static/js/decor.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
+    
+    
+    $("#sviStudenti a,#upisaniStudenti a").on("click",function(e){
+        e.preventDefault();
+        that=$(this);
+        if(that.hasClass("active")){
+            that.removeClass("active");
+        }else{
+            that.addClass("active");
+        }
+    });
 });
