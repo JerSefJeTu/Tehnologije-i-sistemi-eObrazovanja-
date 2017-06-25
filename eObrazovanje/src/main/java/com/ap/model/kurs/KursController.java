4cbcb368cf4f51da8dde7603ab0c17a8321ac0df
Petar Stankov <stankovpetar04@gmail.com>
2017-06-25 17:59:38 +0200

    rutiranje predavaca

--- eObrazovanje/src/main/java/com/ap/model/kurs/KursController.java
+++ eObrazovanje/src/main/java/com/ap/model/kurs/KursController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,8 +31,8 @@ public class KursController {
 		
 	}
 	
-	@RequestMapping(value="/getById", method=RequestMethod.GET)
-	public ResponseEntity<Kurs> getKurs(@RequestParam("id") Long id){
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Kurs> getKurs(@PathVariable Long id){
 		Kurs Kurs = kursService.findOne(id);
 		if(Kurs == null){
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- eObrazovanje/src/main/java/com/ap/model/predispitnaObaveza/PredispitnaObavezaController.java
+++ eObrazovanje/src/main/java/com/ap/model/predispitnaObaveza/PredispitnaObavezaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,8 +28,8 @@ public class PredispitnaObavezaController {
 		
 	}
 	
-	@RequestMapping(value="/getById", method=RequestMethod.GET)
-	public ResponseEntity<PredispitnaObaveza> getKurs(@RequestParam("id") Long id){
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<PredispitnaObaveza> getKurs(@PathVariable Long id){
 		PredispitnaObaveza PredispitnaObaveza = predispitnaObavezaService.findOne(id);
 		if(PredispitnaObaveza == null){
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
