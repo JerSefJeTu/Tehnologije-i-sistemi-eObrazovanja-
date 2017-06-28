f70551ef321f86d710a77bd4fdb4e0a8db13eb0d
Aleksa <domic.aleksa@gmail.com>
2017-06-29 00:50:44 +0200

    Slabo

--- eObrazovanje/src/main/resources/static/js/predavac/predavac.js
+++ eObrazovanje/src/main/resources/static/js/predavac/predavac.js
@@ -1,5 +1,8 @@
 (function (angular) {
 	angular.module('predavac',['pohadjanja.resource','predavac.resource','predmet.resource','student.resource','kurs.resource'])
+    .factory('PredavacFact', function(){
+        
+    })
 	.controller('PredavacCtrl', function($scope, $location,Predmet ,Predavac,$localStorage,$http) {
 		var loadEntries = function () {
 					
@@ -11,13 +14,20 @@
         $scope.alert = function() {
             alert("dodavanje");
         }
-        $scope.brisanjeKursaInit = function(idKursa,naziv) {
-            $scope.nazivKursaInit=naziv;
-            $scope.idKursaInit=idKursa;
-        }
-        $scope.brisanjeKursa = function(){
-            
-        }
+//        $scope.brisanjeKursaInit = function(idKursa,naziv) {
+//            $scope.nazivKursaInit=naziv;
+//            $scope.idKursaInit=idKursa;
+//        }
+//        $scope.brisanjeKursa = function(){
+//            
+//        }
+//        $scope.brisanjeObavezeInit = function(idObaveze, naziv) {
+//            $scope.idObavezeInit = idObaveze;
+//            $scope.nazivObavezeInit = naziv;
+//        }
+//        $scope.brisanjeObaveze = function(){
+//            alert("brise se "+$scope.nazivObavezeInit+", kursa "+$scope.nazivKursaInit);
+//        }
 		$scope.save = function () {
 			if(!$scope.blogEntry._id){
 				$scope.blogEntry.$save(loadEntries);
@@ -49,10 +59,10 @@
 	.controller('kursCtrl', function($scope, $location,Predmet,Student,Pohadjanja ,Kurs,Predavac,$localStorage,$http){
 		
 		$scope.pohadjanje = new Pohadjanja();
-		$scope.izborKursa = function(idKursa){
+		$scope.izborKursa = function(idKursa, naziv){
 	    	
-		      
-		     
+		     $scope.idKursaInit=idKursa;
+             $scope.nazivKursaInit=naziv;
 		      
 		      $scope.sviStudent = new Student.query();
 		      $scope.pohadjanje.$findByKurs({'idKursa':idKursa}).then(function(item){
@@ -74,6 +84,23 @@
 		      
 		        
 		    };
+        $scope.dodavanjeObaveze = function(){
+                console.log($scope.obaveza);
+            }
+        $scope.brisanjeKursaInit = function(idKursa,naziv) {
+            $scope.nazivKursaInit=naziv;
+            $scope.idKursaInit=idKursa;
+        }
+        $scope.brisanjeKursa = function(){
+
+        }
+        $scope.brisanjeObavezeInit = function(idObaveze, naziv) {
+            $scope.idObavezeInit = idObaveze;
+            $scope.nazivObavezeInit = naziv;
+        }
+        $scope.brisanjeObaveze = function(){
+            alert("brise se "+$scope.nazivObavezeInit+", kursa "+$scope.nazivKursaInit);
+        }
 	});
 }(angular));
 
