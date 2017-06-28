1e6ea276372fb1a41ceedf39d1bbcb149d428888
Aleksa <domic.aleksa@gmail.com>
2017-06-28 22:15:23 +0200

    Modali

--- eObrazovanje/src/main/resources/static/js/predavac/predavac.js
+++ eObrazovanje/src/main/resources/static/js/predavac/predavac.js
@@ -9,7 +9,14 @@
 		loadEntries();
 	
         $scope.alert = function() {
-            alert("!!!");
+            alert("dodavanje");
+        }
+        $scope.brisanjeKursaInit = function(idKursa,naziv) {
+            $scope.nazivKursaInit=naziv;
+            $scope.idKursaInit=idKursa;
+        }
+        $scope.brisanjeKursa = function(){
+            
         }
 		$scope.save = function () {
 			if(!$scope.blogEntry._id){
