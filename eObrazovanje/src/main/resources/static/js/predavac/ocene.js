2140656e670b6f5e25be7fee51fcd5165175da38
Stefan Kajkut <stefan.kajkutsf@gmail.com>
2017-06-29 07:08:54 +0200

    date

--- eObrazovanje/src/main/resources/static/js/predavac/ocene.js
+++ eObrazovanje/src/main/resources/static/js/predavac/ocene.js
@@ -1,6 +1,6 @@
 (function (angular) {
-	angular.module('ocene',['pohadjanja.resource','predavac.resource','predmet.resource','student.resource','kurs.resource'])
-	.controller('OceneCtrl', function($scope, $location,Predmet ,Predavac,$localStorage,$http) {
+	angular.module('ocene',['pohadjanja.resource', 'predavac.resource','predmet.resource','student.resource','kurs.resource'])
+	.controller('OceneCtrl', function($scope, $location,Predmet ,Predavac,$localStorage,$http, Pohadjanja) {
 		var loadEntries = function () {
 
 
@@ -9,6 +9,26 @@
 		}
 		loadEntries();
 
+		$scope.pickSubjects = function(predmet) {
+			$scope.selectedCourses = predmet.kursevi;
+		}
+
+		$scope.pickCourseInformation = function(kurs) {
+			$scope.pohadjanja= new Pohadjanja();
+			$scope.pohadjanja.$findByKurs({"idKursa": kurs.id}).then(function(item){
+				console.log(item);
+			})
+			$scope.singleCourse = kurs;
+			var pohadjanja = findAttendingsByCourse(kurs);
+			console.log(pohadjanja);
+			//$scope.numberOfStudents = pohadjanja.data;
+		}
+
+		function findAttendingsByCourse(kurs) {
+			new Pohadjanja().$findByKurs({"idKursa" : kurs.id}).then(function(item){
+				return item;
+			});
+		}
 
 	});
 
